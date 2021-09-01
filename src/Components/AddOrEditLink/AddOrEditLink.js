0d20cd8da0c2062cafe6e856505b2fdaf9cbcc7d
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2021-08-31 20:57:11 -0700

    Add final versions of files

---
+++ src/Components/AddOrEditLink/AddOrEditLink.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const AddOrEditLink = ({
+  setIsEditingLink,
+  addOrEdit,
+  updateLinks,
+  addLink,
+  id,
+}) => {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleAddingLinks = (title, url) => {
+    if (addOrEdit === "add") {
+      addLink({ title, url, clicks: 0 });
+      setTitle('');
+      setUrl('');
+    }
+    if (addOrEdit === "edit") {
+      updateLinks({ title, url, id, clicks: 0 });
+      setIsEditingLink(false);
+    }
+  };
+
+  return (
+    <form className="add-link-widget">
+      <label>Title: </label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <label>URL: </label>
+      <input
+        type="text"
+        id="url"
+        name="url"
+        value={url}
+        onChange={(event) => setUrl(event.target.value)}
+      />
+
+      <button type="button" onClick={() => handleAddingLinks(title, url)}>
+        {addOrEdit === "add" ? "Add" : "Edit"}
+      </button>
+    </form>
+  );
+};
+
+export default AddOrEditLink;
---
+++ src/Components/LinkPreview/LinkPreview.js
@@ -0,0 +1,11 @@
+
+const LinkPreview = ({ link, updateClicks }) => {
+    const handleClick = () => {
+        link.clicks += 1;
+        updateClicks(link)
+    }
+
+    return (<div onClick={handleClick}>{link.title}</div>)
+}
+
+export default LinkPreview;
